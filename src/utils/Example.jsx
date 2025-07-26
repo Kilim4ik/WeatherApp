9d01a0de2ea29ecb77a1c598bcc662e6ba80a646
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2025-07-26 18:57:41 +0200

    fix/styles, prittier's setings, svg reader

--- src/utils/Example.jsx
+++ src/utils/Example.jsx
@@ -1,12 +1,14 @@
-import { createBem } from "../utils/createBem";
-import styles from "./ComponentName.module.scss";
+import { createBem } from "@/utils/createBem";
+import styles from "./example.module.scss";
 
 const bem = createBem("header", styles); //* Створення початкового класу
 
-const ComponentName = () => {
+const Example = () => {
   return (
-    <header className={bem()}> //* Присвоєння початковго класу 
+    <header className={bem()}>
+      //* Присвоєння початковго класу
       <h1 className={bem("title")}>Header</h1> //* Створення вкладеності класу
     </header>
   );
 };
+// export default Example;
