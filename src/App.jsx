682dc10f82075adc5a71f66e2cde6969fe736859
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2025-08-01 11:42:08 +0200

    delete/ button in app for example

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <div className="App">
-      <button className="button--primary">see more</button>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
