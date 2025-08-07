10976aaf6419fec4d0cf89c598529b0809f2f01f
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2025-08-07 08:53:18 +0200

    add context and useLoading

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import { useEffect, useContext } from 'react';
+import { WeatherContext } from './context/weatherContext';
+
 function App() {
-  return <div className="App"></div>;
+  const {
+    cardsArr,
+    handleAddingNewCard,
+    deleteLastCard,
+    addNewCard,
+    dailyForecast,
+    toggleDailyForecast,
+    weeklyForecast,
+    toggleWeeklyForecast,
+    choosenCard,
+    handleChooseCard,
+  } = useContext(WeatherContext);
+
+  useEffect(() => {
+    console.log('cardsArr:', cardsArr);
+    console.log('dailyForecast:', dailyForecast);
+    console.log('weeklyForecast:', weeklyForecast);
+  }, [cardsArr]);
+  return (
+    <div className="App">
+      <button onClick={() => handleAddingNewCard({ card: 'card' })}>add</button>
+      <button onClick={() => toggleDailyForecast()}>dailyForecast</button>
+      <button onClick={() => toggleWeeklyForecast()}>weeklyForecast</button>
+    </div>
+  );
 }
 
 export default App;
