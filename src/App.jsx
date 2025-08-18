1d2ad8f154c9f67bb444b2af7cba8540d76cf636
apenduks1 <k77154313@gmail.com>
2025-08-18 19:08:45 +0300

    first comit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from 'react';
+import Statistic from './components/Statistic';
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <Statistic />
+    </div>
+  );
+};
 
 export default App;
