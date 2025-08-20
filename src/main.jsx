ea5b1799fb4d33e7852bd2f63931c66a46f716c0
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2025-08-20 20:05:57 +0200

    fix/add reset.css in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './style/reset.css';
 import './style/global.scss';
 import App from './App.jsx';
 import { WeatherProvider } from './context/weatherContext.jsx';
