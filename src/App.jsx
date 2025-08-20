c30cde316289f5043cd7402dd23e813635ac2558
apenduks1 <k77154313@gmail.com>
2025-08-20 13:00:44 +0300

    first commikt

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Statistic from './components/Statistic';
-
+import Footer from './components/Footer';
 const App = () => {
   return (
     <div className="app-container">
       <Statistic />
+      <Footer/>
     </div>
   );
 };
